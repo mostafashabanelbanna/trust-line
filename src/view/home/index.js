fa47007198f4604c061cc81e2fc4be0c23b1fda2
Mostafa Shaban <mostafashabanelbanna@gmail.com>
2022-08-29 12:55:55 +0200

    init about

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import About from "./about/About";
 import Hero from "./hero/Hero";
 
 const Home = ({ section1Ref }) => {
   return (
     <div>
-      {" "}
-      <Hero section1Ref={section1Ref} />{" "}
+      <Hero section1Ref={section1Ref} />
+      <About />
     </div>
   );
 };
