ce9db0626137550741a84a13cdb1f8a4d41fd150
Mostafa Shaban <mostafashabanelbanna@gmail.com>
2022-08-30 11:03:16 +0200

    footer

--- src/view/layout/Footer.js
+++ src/view/layout/Footer.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Container, Row, Col, InputGroup, Form, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { AiFillFacebook } from "react-icons/ai";
-import { AiFillInstagram } from "react-icons/ai";
-import { AiFillLinkedin } from "react-icons/ai";
+
+import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 
 import "./footer.css";
@@ -13,12 +12,38 @@ const Footer = () => {
     <>
       <footer>
         <Container>
-          <Row className="border-top p-4">
-            <Col sm={6}>
-              <img src="img/navbar/logo_white.png" />
-              <p>واجهة الرياض -منطقة الأعمال, طريق المطار, الرياض 11564</p>
+          <Row className="border-top py-4 px-md-0 px-2">
+            <Col md={6} className="px-0">
+              <img className="img-fluid my-2" src="img/navbar/logo_white.png" />
+              <div className="my-4">
+                <p className="mb-1">
+                  واجهة الرياض -منطقة الأعمال, طريق المطار, الرياض 11564
+                </p>
+                <p>Trustline - جميع الحقوق محفوظة 2022</p>
+              </div>
             </Col>
-            <Col sm={6}></Col>
+            <Col md={6} className="px-0 d-flex justify-content-between">
+              <div>
+                <ul>
+                  <li>
+                    <Link to="/">للشركات</Link>
+                  </li>
+                  <li>
+                    <Link to="/">للباحثين</Link>
+                  </li>
+                  <li>
+                    <Link to="/">بيانات التواصل</Link>
+                  </li>
+                  <li>
+                    <Link to="/">تواصل معنا</Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <FaLinkedinIn className="mx-4" size={30} color="#fff" />
+                <FaTwitter size={30} color="#fff" />
+              </div>
+            </Col>
           </Row>
         </Container>
       </footer>
